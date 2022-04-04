e48a469a65450bf439c0ce643453263894a2e90d
Christian <christian.fikus@wp.pl>
2022-04-04 21:27:22 +0200

    Included now correctly the Login.js file

---
+++ frontend/src/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+class Login extends React.Component
+{
+    render()
+    {
+        return(
+            <>
+                <Navbar/>
+                This will be a login area, soon
+            </>
+        )
+    }
+}
+
+export default Login
