5a448c89c4f6e30f242742b273cd1c609645ad8d
Karolina Kolek <karolina.kolek@interia.pl>
2022-04-25 19:03:04 +0200

    Made UserException class abstract

--- backend/src/main/java/pl/polsl/shop/user/UserException.java
+++ backend/src/main/java/pl/polsl/shop/user/UserException.java
@@ -1,6 +1,6 @@
 package pl.polsl.shop.user;
 
-public class UserException extends RuntimeException {
+public abstract class UserException extends RuntimeException {
     public UserException(String message){
         super(message);
     }
