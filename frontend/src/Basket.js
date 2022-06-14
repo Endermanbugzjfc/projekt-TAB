3a1794561d9f1ba8aa27c3cddce4d2f5075a6c5e
Christian <christian.fikus@wp.pl>
2022-06-14 20:10:40 +0200

    Made basket prettier and started implementing payment

--- frontend/src/Basket.js
+++ frontend/src/Basket.js
@@ -12,6 +12,20 @@ class Basket extends React.Component
         this.state = {
             products: null
         }
+
+        /**
+         * Cart DTO:
+         *  cart = {
+            id: '',
+            creationDate: '',
+            selectedProducts: [
+                {
+                    id: '',
+                    productDTO: {}
+                }
+            ]
+            }
+         */
     }
 
     componentDidMount()
@@ -30,6 +44,99 @@ class Basket extends React.Component
             .then( response => this.setState({products: response}))
             .catch(err => console.log(err));
         }
+
+        //TEMPORARY
+        var cart = {
+            id: '',
+            creationDate: '',
+            selectedProducts: [
+                {
+                    id: '1',
+                    quantity: 10,
+                    productDTO: {
+                        productId:'1',
+                        name: 'Dachówka',
+                        producer: 'Ja',
+                        description: '1',
+                        category: '1',
+                        inStock: '1',
+                        purchasePrice: '1.40',
+                        retailPrice: '1'
+                    }
+                },
+                {
+                    id: '2',
+                    quantity: 1,
+                    productDTO: {
+                        productId:'2',
+                        name: 'Rynna',
+                        producer: 'Aleksander Wielki',
+                        description: '2',
+                        category: '2',
+                        inStock: '2',
+                        purchasePrice: '2',
+                        retailPrice: '2'
+                    }
+                },
+                {
+                    id: '2',
+                    quantity: 1,
+                    productDTO: {
+                        productId:'2',
+                        name: 'Rynna',
+                        producer: 'Aleksander Wielki',
+                        description: '2',
+                        category: '2',
+                        inStock: '2',
+                        purchasePrice: '2',
+                        retailPrice: '2'
+                    }
+                },
+                {
+                    id: '2',
+                    quantity: 1,
+                    productDTO: {
+                        productId:'2',
+                        name: 'Rynna',
+                        producer: 'Aleksander Wielki',
+                        description: '2',
+                        category: '2',
+                        inStock: '2',
+                        purchasePrice: '2',
+                        retailPrice: '2'
+                    }
+                },
+                {
+                    id: '2',
+                    quantity: 1,
+                    productDTO: {
+                        productId:'2',
+                        name: 'Rynna',
+                        producer: 'Aleksander Wielki',
+                        description: '2',
+                        category: '2',
+                        inStock: '2',
+                        purchasePrice: '2',
+                        retailPrice: '2'
+                    }
+                },
+                {
+                    id: '2',
+                    quantity: 1,
+                    productDTO: {
+                        productId:'2',
+                        name: 'Rynna',
+                        producer: 'Aleksander Wielki',
+                        description: '2',
+                        category: '2',
+                        inStock: '2',
+                        purchasePrice: '2',
+                        retailPrice: '2'
+                    }
+                }
+            ]
+        } 
+        this.setState({products: cart})
     }
 
 
@@ -38,9 +145,12 @@ class Basket extends React.Component
         return(
             <>
                 <Navbar/>
-                <div class="container">
-                    <h1 class="h1 text-center"> Koszyk </h1>
-                    {this.listOfProducts()}
+                <div className="container">
+                    <h1 className="h1 text-center"> Koszyk </h1>
+                    <div className="row">
+                        <div className="col"> {this.listOfProducts()}</div>
+                        <div className="col-3"> {this.payment()} </div>
+                    </div>
                 </div>
             </>
         )
@@ -51,34 +161,90 @@ class Basket extends React.Component
         var returns;
         if(this.state.products != null) 
         {
-            returns = this.state.products.map(prod =>{
-            return this.displayProduct(prod);
-            })
+            returns = <>
+                <div className="table-responsive-sm col">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">Nazwa</th>
+                            <th scope="col">Producent</th>
+                            <th scope="col">Ilość</th>
+                            <th scope="col">Zostało w <br/>magazynie</th>
+                            <th scope="col">Cena za szt.</th>
+                            <th scope="col">Cena całkowita</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.products.selectedProducts.map(prod =>{
+                                return this.displayProduct(prod);
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </>
         } 
         else
-        returns = <><div class="text-center mt-5">
+        returns = <><div className="text-center mt-5">
             Nie masz żadnych produktów w koszyku
         </div></>
         return returns;
+    }
+
+    payment()
+    {
+        var price = 0;
+        this.state.products?.selectedProducts.map(product => price += parseFloat(product.productDTO.retailPrice) * parseFloat(product.quantity) )
+        return <>
+        <form>
+            <div className="mt-5 row">
+                <b>Do zapłaty jest: {price} zł</b>
+            </div>
+            <div className="my-2 row">
+                Wybierz fromę płatności:
+                <div class="btn-group" id="PaymentMethod" role="group" aria-label="Payment radio toggle button group">
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off"/>
+                    <label class="btn btn-outline-dark" for="btnradio1">BLIK</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off"/>
+                    <label class="btn btn-outline-dark" for="btnradio2">Transfer</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off"/>
+                    <label class="btn btn-outline-dark" for="btnradio3">Kartą</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off"/>
+                    <label class="btn btn-outline-dark" for="btnradio4">Przy odbiorze</label>
+                </div>
+            </div>
+            <div className="my-2">
+                <input type="submit" value="Kup" className="btn btn-success" onClick={() => this.Buy()}/>
+            </div>
+        </form>
+
+        </>
     }
 
     displayProduct(product)
     {
         return(
             <>
-                <div class="row offer">
-                    <div class="col">
-                        {product.name} name
-                    </div>
-                    <div class="col my-5" id="emptySpace"></div>
-                    <div class="col me-2"> {product.amount} amount </div>
-                    <div class="col">
-                        {product.price} price
-                    </div>
-
-                </div>
+                <tr id={product.productDTO.productId} key={product.productDTO.productId}>
+                    <td>{product.productDTO.name}</td>
+                    <td>{product.productDTO.producer}</td>
+                    <td>{product.quantity}</td>
+                    <td id={"inStock" + product.productDTO.productId}>{product.productDTO.inStock}</td>
+                    <td>{product.productDTO.retailPrice} zł</td>
+                    <td>{parseFloat(product.productDTO.retailPrice) * parseFloat(product.quantity) } zł</td>
+                    <td> <a className="btn btn-secondary btn-sm" href={"/product/" + product.productDTO.productId}>Zobacz w sklepie</a> </td>
+                </tr>
             </>
         )
+    }
+
+    Buy()
+    {
+        
     }
 
 }
