93af5eeac7db802a162c8d4b57efdce2bb57663d
Karolina Kolek <karolina.kolek@interia.pl>
2022-06-17 18:00:41 +0200

    Added product categories

--- backend/src/main/java/pl/polsl/shop/product/ProductCategory.java
+++ backend/src/main/java/pl/polsl/shop/product/ProductCategory.java
@@ -1,5 +1,19 @@
 package pl.polsl.shop.product;
 
 public enum ProductCategory {
-    //todo some constants
+    ARRANGEMENT ("Aranżacja"), BUILDING ("Budowa"), GARDEN ("Ogród"),
+    INSTALLATION ("Instalacja"), TOOLS_AND_ARTICLES ("Narzędzia i artykuły");
+
+    private final String name;
+
+    private ProductCategory(String name) {
+        this.name = name;
+    }
+
+    public boolean equalsName(String otherName) {
+        return name.equals(otherName);
+    }
+    public String toString() {
+        return this.name;
+    }
 }
